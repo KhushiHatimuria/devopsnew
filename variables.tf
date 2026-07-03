5d5d3be4387b1f048c63e10229ab4c427f8206a3
khushi <149285620+KhushiHatimuria@users.noreply.github.com>
2026-07-03 11:50:56 +0530

    Refactor variables in variables.tf
    Removed descriptions and types for several variables, while adding new variables for vnet, subnet, and vm configurations.

--- variables.tf
+++ variables.tf
@@ -1,35 +1,14 @@
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
+variable "resource_group_name" {}
+variable "location" {}
+variable "subscription_id" {}
 
-variable "resource_group_name" {
-  description = "Resource Group Name"
-  type        = string
-}
+variable "vnet_name" {}
+variable "subnet_name" {}
 
-variable "location" {
-  description = "Azure Region"
-  type        = string
-}
-variable "acr_name" {
-  type = string
-}
+variable "vm_name" {}
+variable "vm_size" {}
 
-variable "aks_name" {
-  type = string
-}
+variable "admin_username" {}
+variable "admin_password" {}
 
-variable "dns_prefix" {
-  type = string
-}
-
-variable "node_count" {
-  type    = number
-  default = 2
-}
-
-variable "vm_size" {
-  type    = string
-  default = "Standard_B2s"
-}
+variable "acr_name" {}
